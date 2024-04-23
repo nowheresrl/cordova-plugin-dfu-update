5cfb02df9b47108a5f4da7a4f214172284b886da
Kevin <bonazzi.kevin@gmail.com>
2024-04-23 16:37:40 +0200

    Update DfuUpdate.d.ts

--- src/ionic/declarations/DfuUpdate.d.ts
+++ src/ionic/declarations/DfuUpdate.d.ts
@@ -3,7 +3,7 @@
 declare module 'cordova-plugin-dfu-update' {
 
     export default class DfuUpdate {
-        static updateFirmware(resultCallback: (result: any) => void, errorCallback: (error: any) => void, options: UpdateOptions);
+        static updateFirmware(resultCallback: (result: any) => void, errorCallback: (error: any) => void, options: UpdateOptions): any;
     }
 
 }
